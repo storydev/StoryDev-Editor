f62225ce997cc04efff2716b9d4042115ceab0e1
tienery <luke@lukeselman.com>
2021-09-30 19:11:20 +0100

    Start on basics for Preview Conversation.

---
+++ StoryDev/Components/PreviewConvoUI.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using StoryDev.Lib.StoryDev;
+
+namespace StoryDev.Components
+{
+    partial class PreviewConvoUI : UserControl
+    {
+
+        private Parser sdParser;
+        private CommandBlock currentBlock;
+
+        private string currentPOV;
+        private int currentBlockIndex;
+        private int currentPosition;
+
+        public PreviewConvoUI()
+        {
+            SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer | ControlStyles.UserPaint, true);
+
+            InitializeComponent();
+
+            sdParser = new Parser();
+        }
+
+        public void ActivateFile(string path)
+        {
+            sdParser.Clear();
+            sdParser.ParseFile(path);
+
+            currentBlockIndex = 0;
+            currentPosition = 0;
+        }
+
+        private void Advance()
+        {
+            
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            var g = e.Graphics;
+
+            g.Clear(Color.FromArgb(128, 206, 237));
+
+            
+        }
+    }
+}
